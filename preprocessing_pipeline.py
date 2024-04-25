96aa6afc6c41ee79c757a974e7fc675be436cd01
Quinn Beck <139178307+Quinn-Beck@users.noreply.github.com>
2024-04-24 20:34:48 -0700

    Update Pipeline/ Start Baseline Performance Comparison

--- preprocessing_pipeline.py
+++ preprocessing_pipeline.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 import torch
 import glob
+from tqdm import tqdm
 
 from PIL import Image
-from torchvision.transforms import Normalize
+from torchvision.transforms.v2 import Normalize
 from torchvision.transforms.functional import to_tensor
 from torch.utils.data import TensorDataset
 
@@ -31,14 +32,13 @@ def get_data(num_classes=10):
     test_data : TensorDataset
         TensorDataset containing labelled testing images.
     """
-    
+    progress_bar = tqdm(range(num_classes*4000))
     # loading all folders and filenames into respective lists
     jpgfolders = glob.glob("C:/Users/Quinn/Desktop/B200C-Lego-Classification/64/*")
     jpgfiles = glob.glob("C:/Users/Quinn/Desktop/B200C-Lego-Classification/64/*/*.jpg")
 
-    # iterate over class folders
+    # iterate over first ``num_classes`` class folders
     for (iter, folder) in enumerate(jpgfolders):
-        print(f"Opening folder {iter}")
         if iter > (num_classes - 1):
             break
         
@@ -50,9 +50,7 @@ def get_data(num_classes=10):
         for (idx, file) in enumerate(glob.glob(f"{folder}/*.jpg")):
             # to_tensor converts PIL image (H x W x C) to a torch.FloatTensor with values in the range [0.0, 1.0] 
             raw_X.append(to_tensor(Image.open(file)))
-            
-            if idx % 1000 == 0:
-                print(f"Converting image {idx}")
+            progress_bar.update(1)
         
         # convert raw_X from a sequence of tensors to torch.tensor with batch dimension len(raw_X)
         raw_X = torch.stack(raw_X)
@@ -72,7 +70,7 @@ def get_data(num_classes=10):
             train_Y = raw_Y
             test_Y = raw_Y[0:800]
         
-        # add the processed contents of current folder to train/ test sets    
+        # add the processed contents of current folder to existing train/ test sets    
         else:
             train_X = torch.cat((train_X, raw_X[0:3200]), dim=0)
             test_X = torch.cat((test_X, raw_X[3200:]), dim=0)
