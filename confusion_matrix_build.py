07e12d3de71b06cccde3cd62642547ffdd76da43
Quinn Beck <139178307+Quinn-Beck@users.noreply.github.com>
2024-05-02 07:57:47 -0700

    Create confusion_matrix_build.py

---
+++ confusion_matrix_build.py
@@ -0,0 +1,27 @@
+import torch
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+
+def confusion_plot(model, data):
+    y_true = []
+    y_pred = []
+    for i, (images, labels) in enumerate(data):
+        images = images.to(device)
+        labels = labels.to(device)
+        
+        x = model(images)
+        value, pred = torch.max(x, 1)
+        
+        y_pred += pred
+        y_true += labels
+
+    confusion_df = pd.DataFrame(confusion_matrix(y_true, y_pred), 
+                                index = [j for j in range(10)],
+                                columns = [j for j in range(10)]
+                               )
+  
+    plt.figure(figsize = (10,7))
+    sns.heatmap(confusion_df, annot=True)
+    plt.show()
