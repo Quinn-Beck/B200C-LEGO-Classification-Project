ab5dce69c8c153ad28b1bcf63a11c44c12c7d01b
Quinn Beck <139178307+Quinn-Beck@users.noreply.github.com>
2024-05-06 14:20:22 -0700

    Update preprocessing_pipeline_v2.py
    Resolved two TODOs. Separated images into train, val, and test sets by class.

--- preprocessing_pipeline_v2.py
+++ preprocessing_pipeline_v2.py
@@ -14,14 +14,28 @@ from torch.utils.data import TensorDataset
 
 def data_generator(num_classes = 10):
     # get list of folder and image file paths
-    # TODO: determine if jpgfolders is needed
-    # TODO: can we string slice the class label from the front of the file path?
     jpgfolders = glob.glob("C:/Users/Quinn/Desktop/B200C-Lego-Classification/64/*")[0:num_classes]
+    # does this return the correct images? Is it needed?
     jpgfiles = glob.glob("C:/Users/Quinn/Desktop/B200C-Lego-Classification/64/*/*.jpg")[0:num_classes*4000]
 
-    for (iter, folder) in enumerate(jpgfolders):
+    label_dict = {f"{jpgfolders[i]}":(i) for i in range(len(jpgfolders))}
+    
+    train = []
+    val = []
+    test = []
+
+    # get file paths for all images in all jpgfolders
+    # and split into train, val, and test sets
+    for folder in jpgfolders:
+        images = glob.glob(f"{folder}/*.jpg")
+        
+        train += images[0:3200]
+        val += images[3200:3600]
+        test += images[3600:4000]
 
     
-    # image paths are shuffled to get a random distribution of classes in each batch
-    random.shuffle(jpgfiles)
+    # image paths are shuffled to ensure a random distribution of classes in each batch
+    random.shuffle(train)
+    random.shuffle(val)
+    random.shuffle(test)
 
