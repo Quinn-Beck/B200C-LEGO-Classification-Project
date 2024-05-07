10472b947050f587bdeb039c8566c83b01c71742
Quinn Beck <139178307+Quinn-Beck@users.noreply.github.com>
2024-05-07 08:46:45 -0700

    Update preprocessing_pipeline_v2.py
    Added torchvision ImageFolder class to handle streaming data

--- preprocessing_pipeline_v2.py
+++ preprocessing_pipeline_v2.py
@@ -5,9 +5,9 @@ import torch
 import glob
 
 from PIL import Image
-from torchvision.transforms.v2 import Normalize
-from torchvision.transforms.functional import to_tensor
+from torchvision.transforms.v2 import Normalize, ToTensor
 from torch.utils.data import TensorDataset
+from torchvision.datasets import ImageFolder
 
 # TODO: ensure a 3200:400:400 train, val, test split for each class
 # TODO: split files in class folder by 3200:400:400, concat then shuffle train, val, and test splits. Pull batches from these splits  
@@ -39,3 +39,22 @@ def data_generator(num_classes = 10):
     random.shuffle(val)
     random.shuffle(test)
 
+    return train, val, test
+
+# yield batches of file paths taking into account end behaviour
+def batcher(data, batch_size)
+    assert len(data) // batch_size - 1 > 0
+    n, r = divmod(len(data), batch_size)
+    for i in range(n):
+        yield data[i*batch_size:(i+1)*batch_size]
+    yield data[-r:]
+
+# utilizing Torch's ImageFolder class to load data!!!
+# in the transforms argument we could pass a custom composition of transforms for on the fly data augmentation
+ImageFolder(root = "C:/Users/Quinn/Desktop/B200C-Lego-Classification/64/*",
+            transform = ToTensor,
+            loader = Image
+           )
+
+# TODO: get equal representation of classes in the data splits, requires preslicing folders somehow
+            
