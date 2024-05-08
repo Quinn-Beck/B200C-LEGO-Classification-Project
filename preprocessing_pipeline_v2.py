1b893eb819ced58028cec460f36df9dc6473eb88
Quinn Beck <139178307+Quinn-Beck@users.noreply.github.com>
2024-05-08 06:59:30 -0700

    Update preprocessing_pipeline_v2.py
    Updated data_generator function to use torchvision's ImageFolder to prepare images for batch streaming. Images are then split into train, validation, and test datasets ensuring equal representation of each class in each split.

--- preprocessing_pipeline_v2.py
+++ preprocessing_pipeline_v2.py
@@ -1,60 +1,22 @@
-import random
-import pandas as pd
-import numpy as np
-import torch
-import glob
-
-from PIL import Image
-from torchvision.transforms.v2 import Normalize, ToTensor
-from torch.utils.data import TensorDataset
+import PIL
+from torch.utils.data import Subset
 from torchvision.datasets import ImageFolder
 
-# TODO: ensure a 3200:400:400 train, val, test split for each class
-# TODO: split files in class folder by 3200:400:400, concat then shuffle train, val, and test splits. Pull batches from these splits  
-
-def data_generator(num_classes = 10):
-    # get list of folder and image file paths
-    jpgfolders = glob.glob("C:/Users/Quinn/Desktop/B200C-Lego-Classification/64/*")[0:num_classes]
-    # does this return the correct images? Is it needed?
-    jpgfiles = glob.glob("C:/Users/Quinn/Desktop/B200C-Lego-Classification/64/*/*.jpg")[0:num_classes*4000]
-
-    label_dict = {f"{jpgfolders[i]}":(i) for i in range(len(jpgfolders))}
+def data_generator(num_classes = 10, t):
+    """
+    Prepare images for batch streaming.
+    """
+    full_data = ImageFolder(root = "C:/Users/Quinn/Desktop/B200C-Lego-Classification/64/*",
+                            transform = t, loader = PIL.Image)
     
-    train = []
-    val = []
-    test = []
+    # get indices to slice the full dataset on to separate training, validation, and testing data
+    # TODO: test that images are read in by ImageFolder sequentially
+    train_ids = [idx for n in range(num_classes) for idx in range(4000*n, 4000*n+3200)]
+    val_ids = [idx for n in range(num_classes) for idx in range(4000*n+3200, 4000*n+3600)]
+    test_ids = [idx for n in range(num_classes) for idx in range(4000*n+3600, 4000*(n+1))]
 
-    # get file paths for all images in all jpgfolders
-    # and split into train, val, and test sets
-    for folder in jpgfolders:
-        images = glob.glob(f"{folder}/*.jpg")
-        
-        train += images[0:3200]
-        val += images[3200:3600]
-        test += images[3600:4000]
+    train_data = Subset(full_data, train_ids)
+    val_data = Subset(full_data, val_ids)
+    test_data = Subset(full_data, test_ids)
 
-    
-    # image paths are shuffled to ensure a random distribution of classes in each batch
-    random.shuffle(train)
-    random.shuffle(val)
-    random.shuffle(test)
-
-    return train, val, test
-
-# yield batches of file paths taking into account end behaviour
-def batcher(data, batch_size)
-    assert len(data) // batch_size - 1 > 0
-    n, r = divmod(len(data), batch_size)
-    for i in range(n):
-        yield data[i*batch_size:(i+1)*batch_size]
-    yield data[-r:]
-
-# utilizing Torch's ImageFolder class to load data!!!
-# in the transforms argument we could pass a custom composition of transforms for on the fly data augmentation
-ImageFolder(root = "C:/Users/Quinn/Desktop/B200C-Lego-Classification/64/*",
-            transform = ToTensor,
-            loader = Image
-           )
-
-# TODO: get equal representation of classes in the data splits, requires preslicing folders somehow
-            
+    return train_data, val_data, test_data
