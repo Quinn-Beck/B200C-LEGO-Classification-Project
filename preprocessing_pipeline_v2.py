fad470a2b8d9d9b1f1a137ab6953ecee9487188e
Quinn Beck <139178307+Quinn-Beck@users.noreply.github.com>
2024-05-14 16:52:33 -0700

    Update preprocessing_pipeline_v2.py
    Fixed the indices for the calls to torchvision.Subset().

--- preprocessing_pipeline_v2.py
+++ preprocessing_pipeline_v2.py
@@ -13,8 +13,8 @@ def data_generator(train_t, eval_t, num_classes = 10):
     full_test_data = ImageFolder(root = "C:/Users/Quinn/Desktop/B200C-Lego-Classification/test",
                             transform = eval_t, loader = PIL.Image)
     
-    train_data = Subset(full_train_data, [0:3200*num_classes])
-    val_data = Subset(full_val_data, [0:400*num_classes])
-    test_data = Subset(full_test_data, [0:400*num_classes])
+    train_data = Subset(full_train_data, range(3200*num_classes))
+    val_data = Subset(full_val_data, range(400*num_classes))
+    test_data = Subset(full_test_data, range(400*num_classes))
     
     return train_data, val_data, test_data
