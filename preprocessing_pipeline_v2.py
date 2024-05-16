7abd3a1a435d34294f600e1fee43295dd86a0551
Quinn Beck <139178307+Quinn-Beck@users.noreply.github.com>
2024-05-15 17:37:40 -0700

    Implemented working training loop
    Trained and evaluated model on full dataset for the first time. With torchvision's ImageFolder, we were able to load data the data, as well as train and evaluate the model in <10 minutes total.

--- preprocessing_pipeline_v2.py
+++ preprocessing_pipeline_v2.py
@@ -6,15 +6,16 @@ def data_generator(train_t, eval_t, num_classes = 10):
     """
     Prepare images for batch streaming.
     """
-    full_train_data = ImageFolder(root = "C:/Users/Quinn/Desktop/B200C-Lego-Classification/train",
-                            transform = train_t, loader = PIL.Image)
-    full_val_data = ImageFolder(root = "C:/Users/Quinn/Desktop/B200C-Lego-Classification/val",
-                            transform = eval_t, loader = PIL.Image)
-    full_test_data = ImageFolder(root = "C:/Users/Quinn/Desktop/B200C-Lego-Classification/test",
-                            transform = eval_t, loader = PIL.Image)
+    train_data = ImageFolder(root = "C:/Users/Quinn/Desktop/B200C-Lego-Classification/train",
+                            transform = train_t)
+    val_data = ImageFolder(root = "C:/Users/Quinn/Desktop/B200C-Lego-Classification/val",
+                            transform = eval_t)
+    test_data = ImageFolder(root = "C:/Users/Quinn/Desktop/B200C-Lego-Classification/test",
+                            transform = eval_t)
     
-    train_data = Subset(full_train_data, range(3200*num_classes))
-    val_data = Subset(full_val_data, range(400*num_classes))
-    test_data = Subset(full_test_data, range(400*num_classes))
+    if num_classes != 200:
+        train_data = Subset(train_data, range(3200*num_classes))
+        val_data = Subset(val_data, range(400*num_classes))
+        test_data = Subset(test_data, range(400*num_classes))
     
     return train_data, val_data, test_data
