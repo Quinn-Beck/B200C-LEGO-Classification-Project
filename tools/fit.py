30e1d7b7550584632416d6dbecc8a7714abf59d7
Quinn Beck <139178307+Quinn-Beck@users.noreply.github.com>
2024-05-21 08:55:14 -0700

    Update fit.py
    Added test function to fit.py which returns macro-averaged precision, recall, and f1 score as well as test set loss and accuracy.

--- tools/fit.py
+++ tools/fit.py
@@ -29,6 +29,45 @@ def train(model, dataloader, optimizer, loss_fn):
     
     return epoch_loss, epoch_acc
 
+def test(model, dataloader, loss_fn):
+    model.eval()
+    counter = 0
+    correct = 0
+    total = len(dataloader.dataset)
+
+    test_running_loss = 0.0
+    all_labels = []
+    all_preds = []
+    with torch.no_grad():
+        for i, (images, labels) in enumerate(dataloader):
+            counter += 1
+            # load data to gpu
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            y = model(images)
+            loss = loss_fn(y, labels)
+            val_running_loss += loss.item()
+            
+            values, preds = torch.max(y, 1)
+            # count correctly classified images
+            correct += torch.sum(preds == labels)
+
+            # Collect all targets and predictions for metric calculations
+            all_labels.extend(labels.view_as(predicted).cpu().numpy())
+            all_preds.extend(preds.cpu().numpy())
+
+    # Calculate overall metrics
+    precision = precision_score(all_labels, all_preds, average='macro')
+    recall = recall_score(all_labels, all_preds, average='macro')
+    f1 = f1_score(all_labels, all_preds, average='macro')
+    
+    # test loss and accuracy for the epoch
+    test_loss = test_running_loss / counter
+    accuracy = 100.0 * (correct / total)
+    
+    return test_loss, accuracy, precision, recall, f1
+
 def validate(model, dataloader, loss_fn):
     model.eval()
     counter = 0
